683785993b19e436684401cd102e73cf6eecf2e2
Azaria <moe9999@protonmail.com>
2022-01-14 23:44:27 +0000

    MFA regex fix

--- src/main/kotlin/me/moe/securityutil/utilities/Constants.kt
+++ src/main/kotlin/me/moe/securityutil/utilities/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     val DEFAULT_PREFIX = System.getenv("BOT_PREFIX") ?: "su!"
     val GITHUB_TOKEN = System.getenv("GITHUB_TOKEN")?: null
 
-    val MFA_TOKEN_REGEX = "/((mfa\\.)?[a-z0-9_-]{20,})/gi".toRegex()
+    val MFA_TOKEN_REGEX = "((mfa\\.)?[a-z0-9_-]{20,})".toRegex(setOf(RegexOption.IGNORE_CASE, RegexOption.MULTILINE))
     val TOKEN_REGEX = "([a-z0-9_-]{23,28})\\.([a-z0-9_-]{6,7})\\.([a-z0-9_-]{27})".toRegex(setOf(RegexOption.IGNORE_CASE, RegexOption.MULTILINE))
     val WEBHOOK_REGEX = "https?:\\/\\/(?:ptb.|canary.|www.|)discord(?:app)?.com\\/api\\/webhooks\\/([0-9]{17,20})\\/([A-Za-z0-9\\.\\-\\_]{60,68})".toRegex(setOf(RegexOption.IGNORE_CASE, RegexOption.MULTILINE))
 
